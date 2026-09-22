8405ebcf1b34c9e9703e17d51cdb44e77b501fb4
borzoitheory <rayasangster@gmail.com>
2026-09-22 20:25:44 +0800

    feat: finalized

---
+++ EmployeeApplication/Employee.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeNameSpace
+{
+    class Employee
+    {
+        private string employeeID;
+        private string employeeFirstName;
+        private string employeeLastName;
+        private string employeePosition;
+
+        public string EmployeeID
+        {
+            get { return employeeID; }
+            set { employeeID = value; }
+        }
+
+        public string EmployeeFirstName
+        {
+            get { return employeeFirstName; }
+            set { employeeFirstName = value; }
+        }
+
+        public string EmployeeLastName
+        {
+            get { return employeeLastName; }
+            set { employeeLastName = value; }
+        }
+
+        public string EmployeePosition
+        {
+            get { return employeePosition; }
+            set { employeePosition = value; }
+        }
+
+        public Employee()
+        {
+            this.employeeID = string.Empty;
+            this.employeeFirstName = string.Empty;
+            this.employeeLastName = string.Empty;
+            this.employeePosition = string.Empty;
+        }
+
+        public Employee(string id, string empFirstName, string empLastName, string empPosition)
+        {
+            this.employeeID = id;
+            this.employeeFirstName = empFirstName;
+            this.employeeLastName = empLastName;
+            this.employeePosition = empPosition;
+        }
+    }
+}
---
+++ EmployeeApplication/frmEmployeeDatabase.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using EmployeeNameSpace;
+
+namespace EmployeeApplication
+{
+    public partial class frmEmployeeDatabase : Form
+    {
+        public frmEmployeeDatabase()
+        {
+            InitializeComponent();
+        }
+
+        private void frmEmployeeDatabase_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void btnSubmit_Click(object sender, EventArgs e)
+        {
+            string id = txtEmployeeID.Text;
+            string empFirstName = txtFirstName.Text;
+            string empLastName = txtLastName.Text;
+            string empPosition = txtPosition.Text;
+
+            Employee emp = new Employee(id, empFirstName, empLastName, empPosition);
+
+            dataGridView1.Rows.Add(emp.EmployeeID, emp.EmployeeFirstName, emp.EmployeeLastName, emp.EmployeePosition);
+        }
+    }
+}
